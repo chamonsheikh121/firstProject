ebf8190cbf08b274b51657f2a53b1d502745c873
Chamon ali <studentchamon1212@gmail.com>
2025-04-23 19:49:28 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+// getting-started.js
+import mongoose from 'mongoose';
+import config from './app/config';
+import app from './app';
+
+async function main() {
+  try {
+    await mongoose.connect(config.databaseUrl as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log('Error connecting to the database', error);
+  }
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+main();
